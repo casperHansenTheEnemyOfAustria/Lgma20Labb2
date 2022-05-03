221eb949d8d6043b72fa827c9162c0688ee6cf5b
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2022-05-03 15:23:42 +0200

    lite mer förklaring

--- avanceradTrapets.py
+++ avanceradTrapets.py
@@ -23,6 +23,7 @@ def trapetsgrafikN(f, presicion, end): #Funktionen tar tre variabler, en funktio
     t = np.linspace(0, end, 100)
     drawFunc(t,f) #Här ritas den faktiska funktionen
     for i in range (0,presicion):
+        #i i detta fallet ger placering typ första rektangeln osv
         x=np.array([i*end/presicion,i*end/presicion,(i+1)*end/presicion,(i+1)*end/presicion]) # för att ge korrekt bas på lådorna beräknas två pukter som basen går emellan och dessa läggs in som vektorer i x koordinaten
         yValue = (f(i*end/presicion)+f((i+1)*end/presicion))/2 #denligt dry principen beräknas trapetsmetodens y-värde här och sätts till en variabel
         y=np.array([0,yValue,yValue,0]) #samma sak händer här med y koordinaten nämnvärt är dock att funktionen här använder sig av trapetsmetoden vilket gör att yvärdet istället beräknas med slutvärde*(f(a)+f(b))/2/presition
